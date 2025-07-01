8fbb94069f20b1c68aadd3be8141ec370374cdbf
B M Rauf <bmrauf@outlook.com>
2025-07-01 08:25:50 +0600

    update

--- Lab Materials/Lab 04 Alternate/excec.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<unistd.h>
-#include<sys/types.h>
-int main(){
-printf("1 \n");
-pid_t pid = fork();
-if(pid == 0) execl("/bin/ls","ls", NULL);
-else if(pid>0) execl("/bin/pwd","pwd", NULL);
-}
